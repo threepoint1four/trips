9ad17801e52fb517f5efe792e3b289cf6460532a
threpoint1four <threepointonefour916@gmail.com>
2026-07-25 21:24:27 -0500

    Setup
    cards and HandEvaluator

---
+++ hand-evaluator/cards.py
@@ -0,0 +1,114 @@
+# cards taken in the form of "AH" (Ace of Hearts), "2D" (Two of Diamonds), etc.
+
+from enum import Enum
+from typing import List
+
+
+class Suit(Enum):
+    HEARTS = "H"
+    DIAMONDS = "D"
+    CLUBS = "C"
+    SPADES = "S"
+
+
+class Rank(Enum):
+    """Card ranks in poker (2-Ace)"""
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
+
+class Card:
+    """Represents a single playing card"""
+    
+    def __init__(self, rank: Rank, suit: Suit):
+        self.rank = rank
+        self.suit = suit
+    
+    def __repr__(self) -> str:
+        rank_str = {
+            Rank.TWO: "2",
+            Rank.THREE: "3",
+            Rank.FOUR: "4",
+            Rank.FIVE: "5",
+            Rank.SIX: "6",
+            Rank.SEVEN: "7",
+            Rank.EIGHT: "8",
+            Rank.NINE: "9",
+            Rank.TEN: "T",
+            Rank.JACK: "J",
+            Rank.QUEEN: "Q",
+            Rank.KING: "K",
+            Rank.ACE: "A"
+        }
+        return (rank_str[self.rank] + self.suit.value)
+
+    def __eq__(self, other) -> bool:
+        if (not isinstance(other, Card)):
+            return NotImplemented
+        return (self.rank == other.rank and self.suit == other.suit)
+
+    def __lt__(self, other) -> bool:
+        if (not isinstance(other, Card)):
+            return NotImplemented
+        return self.rank.value < other.rank.value
+
+class Hand:
+    """Represents a 5-card poker hand"""
+    
+    def __init__(self, cards: List[Card]):
+        if len(cards) != 5:
+            raise ValueError("A hand must consist of exactly 5 cards.")
+        self.cards = sorted(cards, reverse=True)  # Sort cards by rank descending
+    
+    def get_ranks(self) -> List[int]:
+        return [card.rank.value for card in self.cards]
+    
+    def get_suits(self) -> List[str]:
+        return [card.suit.value for card in self.cards]
+
+#class methods
+@classmethod
+def from_string(cls, card_str: str) -> 'Card':
+    """Create a Card instance from a string like 'AH' or '2D'"""
+    rank_map = {
+        "2": Rank.TWO,
+        "3": Rank.THREE,
+        "4": Rank.FOUR,
+        "5": Rank.FIVE,
+        "6": Rank.SIX,
+        "7": Rank.SEVEN,
+        "8": Rank.EIGHT,
+        "9": Rank.NINE,
+        "T": Rank.TEN,
+        "J": Rank.JACK,
+        "Q": Rank.QUEEN,
+        "K": Rank.KING,
+        "A": Rank.ACE
+    }
+    suit_map = {
+        "H": Suit.HEARTS,
+        "D": Suit.DIAMONDS,
+        "C": Suit.CLUBS,
+        "S": Suit.SPADES
+    }
+    rank_char = card_str[0]
+    suit_char = card_str[1]
+    return cls(rank_map[rank_char], suit_map[suit_char])
+
+@classmethod
+def hand_from_listofstrings(card_strs: List[str]) -> Hand:
+    list_of_cards = [Card.from_string(card_str) for card_str in card_strs]
+    return Hand(list_of_cards)
+
+
+    
---
+++ hand-evaluator/evaluator.py
@@ -0,0 +1,147 @@
+from enum import Enum
+from typing import Tuple, List
+from cards import Card, Hand, Rank, Suit
+
+
+class HandRank(Enum):
+    ROYAL_FLUSH = 10
+    STRAIGHT_FLUSH = 9
+    FOUR_OF_A_KIND = 8
+    FULL_HOUSE = 7
+    FLUSH = 6
+    STRAIGHT = 5
+    THREE_OF_A_KIND = 4
+    TWO_PAIR = 3
+    ONE_PAIR = 2
+    HIGH_CARD = 1
+
+
+class HandEvaluator:
+    """Evaluates and ranks poker hands"""
+    
+    @staticmethod
+    def evaluate(hand: Hand) -> Tuple[HandRank, Tuple]:
+        """
+        Evaluate a 5-card hand and return its rank type and tiebreaker values.
+        
+        Returns:
+            Tuple of (HandRank enum, tuple of tiebreaker values)
+            e.g., (HandRank.PAIR, (13, 12, 11, 10))  # Pair of Aces with K, Q, J kickers
+        """
+        # check pairs + trips + quads
+        # i realize some hands do not need kickers but fuck that
+        # i'll just return it anyways; what if the casino sneaks in a few extra cards... you never know!
+        rank_counts = HandEvaluator.get_rank_counts(hand)
+        if 4 in rank_counts.values():
+            return [HandRank.FOUR_OF_A_KIND, (max(k for k, v in rank_counts.items() if v == 4),)]
+        elif sorted(list(rank_counts.values())) == [2, 3]:
+            return [HandRank.FULL_HOUSE, (max(k for k, v in rank_counts.items() if v == 3), max(k for k, v in rank_counts.items() if v == 2))]
+        elif 3 in rank_counts.values():
+            return [HandRank.THREE_OF_A_KIND, (max(k for k, v in rank_counts.items() if v == 3),)]
+        elif list(rank_counts.values()).count(2) == 2:
+            return [HandRank.TWO_PAIR, tuple(sorted([k for k, v in rank_counts.items() if v == 2], reverse=True))]
+        elif 2 in rank_counts.values():
+            return [HandRank.ONE_PAIR, (max(k for k, v in rank_counts.items() if v == 2),)]
+
+        # check flush & straight
+        flush_checker = HandEvaluator.is_flush(hand)
+        straight_checker = HandEvaluator.is_straight(hand)
+
+        # returns for royal flush, straight flush, flush, 
+        
+        if (HandEvaluator.is_flush(hand) and HandEvaluator.is_straight(hand)[0] and HandEvaluator.is_straight(hand)[1] == 14):
+            return [HandRank.ROYAL_FLUSH, [1e9]]
+        elif (HandEvaluator.is_flush(hand) and HandEvaluator.is_straight(hand)[0]):
+            return [HandRank.STRAIGHT_FLUSH, (HandEvaluator.is_straight(hand)[1],)]
+        elif (HandEvaluator.is_flush(hand) and not HandEvaluator.is_straight(hand)[0]):
+            return [HandRank.FLUSH, tuple(sorted([k for k in rank_counts.keys()], reverse=True))]
+        elif (not HandEvaluator.is_flush(hand) and HandEvaluator.is_straight(hand)[0]):
+            return [HandRank.STRAIGHT, (HandEvaluator.is_straight(hand)[1],)]
+
+        # if none of the above cases are satisfied, return high card with sorted kickers
+        return [HandRank.HIGH_CARD, tuple(sorted([k for k in rank_counts.keys()], reverse=True))]
+
+    @staticmethod
+    def is_flush(hand: Hand) -> bool:
+        """
+        Check if the hand is a flush
+
+        Returns: True/False
+
+        """
+        list_of_suits = hand.get_suits()
+        return len(set(list_of_suits)) == 1
+    
+    @staticmethod
+    def is_straight(hand: Hand) -> Tuple[bool, int]:
+        """
+        Check if the hand is a straight.
+        
+        Returns:
+            Tuple of (True/False, Kicker Rank)
+        """
+        ranks = hand.get_ranks()
+        ranks.sort()
+        # Handle Ace-low straight (A-2-3-4-5)
+        if ranks == [2, 3, 4, 5, 14]:  # Ace is high (14) but can be low in this case
+            return True, 5
+        for i in range(4):
+            if (ranks[i] + 1 != ranks[i + 1]):
+                return False, -1
+        return True, ranks[-1]
+    
+    @staticmethod
+    def get_rank_counts(hand: Hand) -> dict:
+        """
+        Count occurrences of each rank.
+        
+        Returns:
+            Dict mapping rank -> count, e.g., {13: 2, 12: 1, 11: 1, 10: 1}
+        """
+        rank_counts = {}
+        for card in hand.cards:
+            rank_counts[card.rank.value] = rank_counts.get(card.rank.value, 0) + 1
+        return rank_counts
+    
+    @staticmethod
+    def compare_hands(hand1: Hand, hand2: Hand) -> int:
+        """
+        Compare two hands.
+        
+        Returns:
+            1 if hand1 is better
+            -1 if hand2 is better
+            0 if hands are equal
+        """
+        hr1 = HandEvaluator.evaluate(hand1)[0]
+        hr2 = HandEvaluator.evaluate(hand2)[0]
+        if (hr1>hr2):
+            return 1
+        elif (hr1<hr2):
+            return -1
+        else: 
+            # If hand ranks are equal, compare tiebreakers
+            tiebreaker1 = HandEvaluator.evaluate(hand1)[1]
+            tiebreaker2 = HandEvaluator.evaluate(hand2)[1]
+            if tiebreaker1 > tiebreaker2:
+                return 1
+            elif tiebreaker1 < tiebreaker2:
+                return -1
+            else:
+                return 0
+
+
+def parse_card(card_str: str) -> Card:
+    """
+    Parse a card string like 'AH' (Ace of Hearts) into a Card object.
+    Supports formats: AH, 2D, KS, 10C, etc.
+    """
+    return Card.from_string(card_str)
+    # yeah this does the same thing as from_string but whatever, it's probably a bit nicer
+
+
+def find_best_hand(community_cards: List[Card], hole_cards: List[Card]) -> Hand:
+    """
+    Given 5 community cards and 2 hole cards (7 total), find the best 5-card hand.
+    """
+    pass
