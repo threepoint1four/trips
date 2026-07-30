7c17a040def3a9ff969b15fa4a2c7325fcc0e333
threpoint1four <threepointonefour916@gmail.com>
2026-07-30 11:24:59 -0500

    multiplayer hand evaluator

--- hand-evaluator/test.py
+++ hand-evaluator/test.py
@@ -1,7 +1,9 @@
 from cards import Card, Hand, Rank, Suit
-from evaluator import HandEvaluator, HandRank
+from evaluator import HandEvaluator, find_best_hand, parse_card
 
 # two hand comparison test
+
+"""
 
 hand1 = Hand([Card.from_string(card_str) for card_str in input("Input 5 cards for the first hand: ").split()])
 [hand_rank, tiebreaker] = HandEvaluator.evaluate(hand1)
@@ -25,3 +27,54 @@ elif (checker == -1):
 else:
     print("It's a tie!" + (f" Both hands are a {rank_name} :) "))
 
+"""
+
+# best hand finder test with 1 person
+"""
+community_cards = [Card.from_string(card_str) for card_str in input("Input 5 community cards: ").split()]
+hole_cards = [Card.from_string(card_str) for card_str in input("Input 2 hole cards: ").split()]
+best_hand = find_best_hand(community_cards, hole_cards)
+hand_rank, tiebreaker = HandEvaluator.evaluate(best_hand)
+print(f"Best Hand: {', '.join(str(card) for card in best_hand.cards)}")
+print(f"Rank: {hand_rank.name.replace('_', ' ').title()} with tiebreaker(s): {tiebreaker}")
+
+"""
+
+# best hand finder test with multiple players
+community_cards = [Card.from_string(card_str) for card_str in input("Input 5 community cards: ").split()]
+num_players = int(input("Input number of players: "))
+hole_card_list = []
+for i in range (num_players):
+    hole_cards = [Card.from_string(card_str) for card_str in input(f"Input player {i+1}'s cards: ").split()]
+    hole_card_list.append(hole_cards)
+optimal_hands = [find_best_hand(community_cards, hole_cards) for hole_cards in hole_card_list]
+
+evaluations = [HandEvaluator.evaluate(hand) for hand in optimal_hands]
+best_eval = evaluations[0]
+winning_indices = [0]
+
+for index in range(1, len(evaluations)):
+    current_eval = evaluations[index]
+    comparison = HandEvaluator.compare_hands(optimal_hands[index], optimal_hands[winning_indices[0]])
+    if comparison == 1:
+        best_eval = current_eval
+        winning_indices = [index]
+    elif comparison == 0:
+        winning_indices.append(index)
+
+if len(winning_indices) == 1:
+    # one winner
+    winner_index = winning_indices[0]
+    hand_rank, tiebreaker = evaluations[winner_index]
+    rank_name = hand_rank.name.replace("_", " ").title()
+    print(f"Player {winner_index + 1} wins with hand: {', '.join(str(card) for card in optimal_hands[winner_index].cards)}")
+    print(f"Rank: {rank_name} | Tiebreaker: {tiebreaker}")
+else:
+    # handle ties
+    winners_list = [index + 1 for index in winning_indices]
+    hand_rank, tiebreaker = evaluations[winning_indices[0]]
+    rank_name = hand_rank.name.replace("_", " ").title()
+    print(f"Players {', '.join(str(winner) for winner in winners_list)} tie with hand rank: {rank_name}")
+    print(f"Tiebreaker: {tiebreaker}")
+
+    
